7446e359a0f6ea28a16cd5c902e77ae830db69b4
mohamed <mohamedsahel55@gmail.com>
2021-02-13 08:57:38 +0100

    get form submitting loader functionality back

--- js/form.js
+++ js/form.js
@@ -1,184 +1,3 @@
-// const formElement = select('.search_form')
-
-// const fields = [
-//   {
-//     name: 'postcode',
-//     event: 'input'
-//   },
-//   {
-//     name: 'type_terrein',
-//     event: 'change'
-//   },
-//   {
-//     name: 'situatie',
-//     event: 'change'
-//   }
-// ]
-
-
-// const getField = function (name) {
-//   return form.querySelectorAll(`.form_step[data-name="${name}"]`)[0]
-// }
-
-// class Form {
-//   constructor(form) {
-//     this.form = form
-//     this.fields = []
-//   }
-
-//   canSubmit() {
-//     let errors = []
-
-//     this.fields.forEach(field => {
-//       if(field.name === 'postcode' && !checkPostcode(field.input.value)) {
-//         errors.push(field)
-//       } else if(!field.input.value) {
-//         errors.push(field)
-//       }
-//     })
-
-//     if(!errors.length) {
-//       errors = false
-//     }
-
-//     return { errors }
-//   }
-
-
-
-//   render = () => {
-//     const formStorage = JSON.parse(localStorage.getItem('search_params'))
-//     console.log(formStorage)
-
-//     this.fields = fields.map((field, index) => {
-//       field = new Field({ ...field, index, nextField: fields[index + 1] })
-//       field.render()
-
-//       return field
-//     })
-
-//     if (formStorage) {
-//       this.form.classList.add('opened')
-//       this.fields[this.fields.length - 1].nextBtn.disabled = false
-//     } else {
-//       this.fields.forEach((field) => {
-//         if(field.index > 0) {
-//           field.field.classList.add('hidden')
-//         }
-//       })
-//     }
-
-//     form.addEventListener('submit', (e) => {
-//       e.preventDefault()
-//       const { errors } = this.canSubmit()
-
-//       if(!errors) {
-//         form.submit()
-//         form.classList.add('submitting')
-//       } else {
-//         // e.preventDefault()
-//         errors.forEach((error, index) => {
-//           error.field.classList.add('error')
-
-//           if(index === 0) {
-//             error.field.scrollIntoView({
-//               behavior: 'smooth',
-//               block: 'center'
-//             })
-//           }
-//         })
-//       }
-//     })
-
-//     localStorage.removeItem('search_params')
-//   }
-// }
-
-
-
-// class Field {
-//   constructor({ name, index, event, nextField }) {
-//     // elements
-//     this.field = getField(name),
-//     this.nextBtn = this.field.querySelector('.next'),
-//     this.input = document.forms[0][name],
-//     this.pickedValueSpan = this.field.querySelector('.step_picked-value span'),
-//     this.editBtn = this.field.querySelector('.step_edit-icon')
-
-//     this.name = name
-//     this.event = event
-//     this.index = index
-//     this.nextField = nextField
-//     this.canNext = false
-//     // rules
-//   }
-
-
-//   // Input Change Handler
-//   onInput = (e) => {
-//     const value = this.input.value
-
-//     if(this.name === 'postcode') {
-//       if (checkPostcode(value)) {
-//         this.field.classList.remove('error')
-//       }
-//       this.canNext = value.length >= 4
-//     } else {
-//       this.canNext = !!value
-//     }
-
-//     if (this.canNext) {
-//       this.nextBtn.disabled = false
-//     } else {
-//       this.nextBtn.disabled = true
-//     }
-//   }
-
-//   // Next Handler
-//   onNext = () => {
-//     let value = this.input.value
-
-//     if(!this.nextField) return
-
-//     if (this.name === 'postcode') {
-//       if(checkPostcode(value)) {
-//         value = value.toUpperCase()
-//       } else {
-//         this.field.classList.add('error')
-//         return
-//       }
-//     }
-
-
-//     this.field.classList.add('passed')
-//     this.pickedValueSpan.textContent = value
-//     // check if it is not the last fied
-//     getField(this.nextField.name).classList.remove('hidden')
-//   }
-
-//   onEdit = () => {
-//     this.field.classList.remove('passed')
-//     this.field.classList.add('editing')
-//   }
-
-//   render() {
-//     this.field.addEventListener(this.event, this.onInput)
-//     this.nextBtn.addEventListener('click', this.onNext)
-//     this.editBtn.addEventListener('click', this.onEdit)
-
-//     const formStorage = JSON.parse(localStorage.getItem('search_params'))
-
-//     if(formStorage) {
-//       this.input.value = formStorage[this.name]
-//     }
-//   }
-
-// }
-
-// const form = new Form(formElement)
-
-// form.render()
-
 "use strict";
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
@@ -246,16 +65,14 @@ var Form = /*#__PURE__*/function () {
         });
       }
       _this.form.addEventListener('submit', function (e) {
-        // e.preventDefault();
-
         var _this$canSubmit = _this.canSubmit(),
             errors = _this$canSubmit.errors;
 
         if (!errors) {
-          // _this.form.submit();
           _this.form.classList.add('submitting');
         } else {
           e.preventDefault()
+          console.log(errors)
           errors.forEach(function (error, index) {
             error.field.classList.add('error');
 
@@ -280,10 +97,15 @@ var Form = /*#__PURE__*/function () {
     value: function canSubmit() {
       var errors = [];
       this.fields.forEach(function (field) {
-        if (field.name === 'postcode' && !checkPostcode(field.input.value)) {
-          errors.push(field);
-        } else if (!document.querySelectorAll('input[name=' + field.name + ']:checked')[0].value) {
-          errors.push(field);
+        if (field.name === 'postcode') {
+          if(!checkPostcode(field.input.value)) {
+            errors.push(field);
+          }
+        } else {
+          var fieldChoise = document.querySelectorAll('input[name=' + field.name + ']:checked')[0]
+          if (!fieldChoise || !fieldChoise.value) {
+            errors.push(field);
+          }
         }
       });
 
